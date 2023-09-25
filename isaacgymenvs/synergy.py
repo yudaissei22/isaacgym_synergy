70420622b1a66826577c29361e98e5e3754aaf4a
yuda <yuda@jsk.imi.i.u-tokyo.ac.jp>
2023-09-25 22:42:30 +0900

    iikanzi?

--- isaacgymenvs/synergy.py
+++ isaacgymenvs/synergy.py
@@ -1,11 +1,15 @@
 from isaccgym import gymutil, gymapi, gymtorch
 import numpy as np
+import yaml # i dont know where the yaml file loaded in program
 
-class Model(Vectask):
+class Model:
     def __init__(self, cfg, args):
         self.cfg = cfg
 
     def create_sim(self, args):
         sim_params = gymapi.SimParams()
         
-        """ read from yaml file is better? """
+        """ read from yaml file """
+        sim_params.dt = self.dt
+        if self.cfg["sim"]["up_axis"] == "z":
+            sim_params.up_axis = gymapi_.UP_AXIS_Z
